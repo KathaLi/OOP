dc01f896109b306340a8eabb2c700e6b6ad416c9
katha <katharina.linhoff@iu-study.org>
2025-03-28 20:47:01 +0100

    added child classes

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 # This is a sample Python script.
+from recipes import Recipe, MainDishRecipe
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+recipe = Recipe("Test", 10, "beginner")
+print(recipe.display_recipe())
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
+main_dish = MainDishRecipe("Main", 10, "beginner", cooking_time = 10)
+main_dish.is_vegetarian = True
+main_dish.is_vegan = True
+print(main_dish.display_recipe())
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- recipes.py
+++ recipes.py
@@ -1,24 +1,110 @@
+class IngredientItem:
+    def __init__(self, name, amount, calories, carbs, fat, sugar):
+        self.name = name
+        self.amount = amount
+        self.calories = calories
+        self.carbs = carbs
+        self.fat =fat
+        self.sugar = sugar
+
+
+
+
 ### This script creates the parent, abstract class recipe
 class Recipe:
-    def __init__(self, name, prep_time, level, is_meal_prep = False):
+    def __init__(self, name, prep_time, level):
         self.name = name
         #prep_time in min
         self.prep_time = prep_time
         self.is_meal_prep = False
         self.level = level
+        self.ingredients = [
+            IngredientItem(name = "broccoli", amount = 1, calories = 100, carbs = 2, fat=3, sugar=2),
+            IngredientItem(name = "noodles", amount = 100, calories = 200, carbs = 4, fat=3, sugar=2),
+            IngredientItem(name = "garlic", amount = 50, calories = 15, carbs = 8, fat=3, sugar=2),
+        ]
         #self.ingredients = ingredients
     def display_recipe(self):
         print(f"{self.name} for {self.level} \n")
-        print(f"{self.prep_time} minutes \n")
+        print(f"Prep time:{self.prep_time} minutes \n")
         if self.is_meal_prep:
-            print(f"{self.is_meal_prep} \n")
+            print(f"Perfect for meal prep \n")
     def calculate_calories(self):
-        calories = 50
+        calories = 0
+        for ingredient in self.ingredients:
+            calories += ingredient.calories
         return calories
     def calculate_total_time(self):
-        total_time = self.prep_time
+       return self.prep_time
     def calculate_rating(self):
-        rating = 10
+        return 10
 
-recipe = Recipe("Test", 10, "beginner")
-print(recipe.display_recipe())
+
+
+class MainDishRecipe(Recipe):
+    def __init__(self, name, prep_time, level,cooking_time):
+        Recipe.__init__(self, name = name, prep_time = prep_time, level = level)
+        self.cooking_time = cooking_time
+        self.is_vegetarian = False
+        self.is_vegan = False
+        self.need_oven = False
+    def display_recipe(self):
+        print(f"{self.name} for {self.level} \n")
+        print(f"Prep time:{self.prep_time} minutes \n")
+        if self.is_meal_prep:
+            print(f"Perfect for meal prep \n")
+        if self.is_vegetarian and (self.is_vegan == False):
+            print(f"Suitable for a vegetarian diet \n")
+        if self.is_vegan:
+            print(f"Suitable for a vegan diet \n")
+    def calculate_carbs(self):
+        carbs = 0
+        for ingredient in self.ingredients:
+            carbs += ingredient.calories
+        return carbs
+    def calculate_total_time(self):
+        return self.prep_time + self.cooking_time
+
+class DessertRecipe(Recipe):
+    def __init__(self, name, prep_time, level,cooling_time):
+        Recipe.__init__(self, name = name, prep_time = prep_time, level = level)
+        self.cooling_time = cooling_time
+        self.contains_nuts = False
+    def display_recipe(self):
+        print(f"{self.name} for {self.level} \n")
+        print(f"Prep time:{self.prep_time} minutes \n")
+        if self.is_meal_prep:
+            print(f"Perfect for meal prep \n")
+        if not self.contains_nuts:
+            print(f"Suitable for a nut allergies \n")
+    def calculate_fat(self):
+        fat = 0
+        for ingredient in self.ingredients:
+            fat += ingredient.sugar
+        return fat
+    def calculate_sugar(self):
+        sugar = 0
+        for ingredient in self.ingredients:
+            sugar += ingredient.sugar
+            return sugar
+    def calculate_total_time(self):
+        return self.prep_time +self.cooling_time
+
+class CakeRecipe(DessertRecipe):
+    def __init__(self, name, prep_time, level,cooling_time, baking_time):
+        DessertRecipe.__init__(self, name = name, prep_time = prep_time, level = level, cooling_time = cooling_time)
+        self.baking_time = baking_time
+    def calculate_total_time(self):
+        return self.prep_time + self.cooling_time + self.baking_time
+    def display_recipe(self):
+        print(f"{self.name} for {self.level} \n")
+        print(f"Prep time:{self.prep_time} minutes \n")
+        if self.is_meal_prep:
+            print(f"Perfect for meal prep \n")
+        if not self.contains_nuts:
+            print(f"Suitable for a nut allergies \n")
+
+
+
+
+
